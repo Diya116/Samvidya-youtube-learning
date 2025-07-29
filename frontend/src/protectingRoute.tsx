a983d6a6ba60967fce3a02b22a0207914d58f155
Diya@116 <dps1162004@gmail.com>
2025-07-29 13:10:31 +0530

    fixed: isAuthenticated function use in protectingRoute file

--- frontend/src/protectingRoute.tsx
+++ frontend/src/protectingRoute.tsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { isAuthenticated } = useAuth();
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated()) {
     return <Navigate to="/auth/login" />;
   }
 
