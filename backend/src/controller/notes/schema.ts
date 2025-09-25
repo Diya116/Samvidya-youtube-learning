1840ffa1ae358423b0bd00a2db25b6c3bd95ae48
Diya@116 <dps1162004@gmail.com>
2025-09-25 13:36:05 +0530

    added notes by courseid endpoint

--- backend/src/controller/notes/schema.ts
+++ backend/src/controller/notes/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 export const noteSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   content: z.string(),
-  lessonId:z.string().optional()
+  courseId:z.string().optional()
 });
 // Infer the TypeScript type from the schema
 export type noteType = z.infer<typeof noteSchema>;
